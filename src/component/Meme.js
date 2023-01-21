ceb07301741a63a3a3fcb79e28def9f4048013a3
Anshuman <anshuman.si22@gmail.com>
2023-01-21 21:17:59 +0530

    meme update

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,14 +1,33 @@
 import React from "react";
+import memeData from "../memeData";
+/**
+ * Challenge: Get a random image from the `memesData` array
+ * when the "new meme image" button is clicked.
+ *
+ * Log the URL of the image to the console. (Don't worry
+ * about displaying the image yet)
+  const randomIndex = Math.floor(Math.random() * arr.length);
+*/
 
 function Meme() {
+  let randomUrl;
+  function randomImageGen() {
+    const memeArr = memeData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memeArr.length);
+
+    randomUrl = memeArr[randomIndex].url;
+    console.log(randomUrl);
+  }
+
   return (
     <main className="meme-container">
-      <form className="input-box">
+      <p>{randomUrl}</p>
+      <div className="input-box">
         <input type="text" id="text-top" placeholder="Top text" />
         <input type="text" id="text-bottom" placeholder="Bottom text" />
-      </form>
+      </div>
       <div className="button-box">
-        <button type="submit" className="submit-btn">
+        <button onClick={randomImageGen} type="submit" className="submit-btn">
           Get a new meme image 🖼️
         </button>
       </div>
